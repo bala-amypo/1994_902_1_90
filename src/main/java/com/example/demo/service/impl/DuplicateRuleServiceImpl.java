78e6d9ec8c3d97fe7f765b253d7bc05ac0f9c799
bala-amypo <bala@amypo.in>
2025-12-22 03:39:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DuplicateRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DuplicateRuleServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.demo.service.impl;
 import com.example.demo.service.DuplicateRuleService;
 import com.example.demo.entity.DuplicateRule;
 import com.example.demo.repository.DuplicateRuleRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.service;
 import java.util.List;
 
 @Service
