4cea1b26a6414fcd5f46d300da4a2b683df182cd
bala-amypo <bala@amypo.in>
2025-12-23 08:46:07 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketCategoryService.java
+++ src/main/java/com/example/demo/service/TicketCategoryService.java
@@ -1,10 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.TicketCategory;
+
 import java.util.List;
 
 public interface TicketCategoryService {
-    TicketCategory createCategory(TicketCategory category);
-    List<TicketCategory> getAllCategories();
-    TicketCategory getCategory(Long id);
+
+    TicketCategory save(TicketCategory category);
+
+    List<TicketCategory> getAll();
+
+    TicketCategory getById(Long id);
 }
--- src/main/java/com/example/demo/service/impl/TicketCategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TicketCategoryServiceImpl.java
@@ -1,7 +1,33 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.TicketCategory;
+import com.example.demo.repository.TicketCategoryRepository;
+import com.example.demo.service.TicketCategoryService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class TicketCategoryServiceImpl {
+public class TicketCategoryServiceImpl implements TicketCategoryService {
+
+    private final TicketCategoryRepository repository;
+
+    public TicketCategoryServiceImpl(TicketCategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public TicketCategory save(TicketCategory category) {
+        return repository.save(category);
+    }
+
+    @Override
+    public List<TicketCategory> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public TicketCategory getById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
