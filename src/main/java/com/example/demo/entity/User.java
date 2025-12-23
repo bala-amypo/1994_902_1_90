2dc1baaaec549a712fb6a913c6aa57eaf21448fd
bala-amypo <bala@amypo.in>
2025-12-23 09:58:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -8,12 +8,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String fullName;
+    private String password;
+    private String role;
 
     @Column(unique = true)
     private String email;
 
     public Long getId() {
         return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(){
+        this.
     }
 
     public String getEmail() {
